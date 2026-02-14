7c2d25140e4ecb93e4528f1616fe634090dd7811
Md Shaquib Khan <mdshaquibkhan@Mds-MacBook-Air.local>
2026-02-14 11:56:44 +0530

    added-changes-versel

--- whatsapp-groups-backend/src/server.ts
+++ whatsapp-groups-backend/src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import groupRoutes from './routes/group.routes'
-import { errorHandler } from '../src/middleware/error.middleware'
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import serverless from "serverless-http"
+
+import groupRoutes from "./routes/group.routes"
+import { errorHandler } from "./middleware/error.middleware"
 
 dotenv.config()
 
@@ -11,13 +13,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/test', (req, res) => {
-  res.send('Server working');
-});
+app.get("/test", (req, res) => {
+  res.send("Server working")
+})
 
-app.use('/api/groups', groupRoutes)
+app.use("/api/groups", groupRoutes)
 
 app.use(errorHandler)
 
-
-export default app
+export const handler = serverless(app)
