e558410fb802693042ac5e6bc8e0b67aed8abd86
Md Shaquib Khan <mdshaquibkhan@Mds-MacBook-Air.local>
2026-02-14 19:35:10 +0530

    cors-error-solved

--- whatsapp-groups-backend/src/server.ts
+++ whatsapp-groups-backend/src/server.ts
@@ -9,7 +9,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // allow all (for testing)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
+
+app.options("*", cors())
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
